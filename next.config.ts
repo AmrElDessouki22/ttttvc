15c7d8edce6645e85f2d65109db22eb62681b7ab
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-26 23:13:07 +0300

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: false
+  },
+  eslint: {
+    ignoreDuringBuilds: false
+  }
+}
+export default nextConfig
